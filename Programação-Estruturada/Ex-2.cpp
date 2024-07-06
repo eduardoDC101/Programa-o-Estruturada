6a177502bbd02c2301f1b6079e5609a78e56519c
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2024-07-06 14:59:54 -0300

    Update Ex-2.cpp

--- Programação-Estruturada/Ex-2.cpp
+++ Programação-Estruturada/Ex-2.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
-
 using namespace std;
-
 int main()
 {
     int n1,n2,n3;
-
     cout << "Digite o primeiro numero: " << endl;
     cin >> n1;
     cout << "Digite o segundo numero: " << endl;
     cin >> n2;
     cout << "Digite o terceiro numero: " << endl;
     cin >> n3;
-
      if(n1 > n2 && n3)
      {
         cout << "O maior numero e " << n1;
@@ -26,7 +22,6 @@ int main()
      {
         cout << "O maior numero e " << n3;
      }
-
  return 0;
 }
 
