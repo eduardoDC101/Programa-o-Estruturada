c602a4c332cc21ec16a23f5097e581a43c3d501e
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2024-07-07 18:57:35 -0300

    Update Ex-7.cpp

--- Programação-Estruturada/Ex-7.cpp
+++ Programação-Estruturada/Ex-7.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 #include <cstdlib>
-
 using namespace std;
-
-
 int main(int argc, char** argv)
 {
    char nome[50];
    int nasc,i,idade,idadet;
-
    idadet = 0;
-
    for (i = 1; i <= 5; i++)
    {
     cout << "Informe o nome: " << endl;
     cin >> nome;
     cout << "Infomre o ano de nascimento: " << endl;
     cin >> nasc;
-
     idade = 2024 - nasc;
     cout << "Essa pessoa tem " << idade << " anos" << endl;
     cout << "-------------------------" << endl;
